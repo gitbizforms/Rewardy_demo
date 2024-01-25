d44bdf087727d4802f8be387a161a7d5373a189d
wtDrago <suyong9851@gmail.com>
2024-01-25 12:23:48 +0900

    데모 파일 정리

--- mysql_db/work_cp_title.sql
+++
@@ -1,7 +0,0 @@
-INSERT work_cp_title ( state, kind, title, sort, code, regdate) VALUES (0, N'', N'적극성', NULL, NULL, CAST(N'2022-01-25T17:45:25.187' AS DateTime));
-INSERT work_cp_title ( state, kind, title, sort, code, regdate) VALUES (0, N'', N'협업', NULL, NULL, CAST(N'2022-01-25T17:45:31.140' AS DateTime));
-INSERT work_cp_title ( state, kind, title, sort, code, regdate) VALUES (0, N'', N'셀프리더십', NULL, NULL, CAST(N'2022-01-25T17:45:37.780' AS DateTime));
-INSERT work_cp_title ( state, kind, title, sort, code, regdate) VALUES (0, N'', N'성실', NULL, NULL, CAST(N'2022-01-25T17:45:43.060' AS DateTime));
-INSERT work_cp_title ( state, kind, title, sort, code, regdate) VALUES (0, N'', N'노하우', NULL, NULL, CAST(N'2022-01-25T17:45:48.280' AS DateTime));
-INSERT work_cp_title ( state, kind, title, sort, code, regdate) VALUES (0, N'', N'성과', NULL, NULL, CAST(N'2022-01-25T17:45:53.327' AS DateTime));
-
