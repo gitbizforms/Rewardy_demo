fef6eead45d3afaa17aeafa899d941247c5e9398
wtDrago <suyong9851@gmail.com>
2024-02-06 10:01:59 +0900

    캐시 수정

--- inc_lude/dbcon_mysqli.php
+++ inc_lude/dbcon_mysqli.php
@@ -1,18 +1,18 @@
 <?
 	//서버주소
-	$serverName = "10.17.239.90";
+	$serverName = "14.63.161.43";
 
 	//데이터베이스
-	$dbcon_Database = "todaywork_demo";
+	$dbcon_Database = "todaywork";
 
 	//계정
-	$dbcon_UID = "todaywork_demo";
+	$dbcon_UID = "todaywork";
 
 	//패스워드
 	$dbcon_pass = "work2021%%^^";
 
 	//포트
-	$dbcon_port = "3306";
+	$dbcon_port = "13306";
 
 	//DB연결
 	$conn = mysqli_connect($serverName, $dbcon_UID, $dbcon_pass, $dbcon_Database, $dbcon_port) or die("db connect fail");
